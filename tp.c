15d95d1f313ecdc800eb2e97da012d88f6fbf02d
oshironadina <noshiro@fi.uba.ar>
2026-06-20 22:59:36 -0300

    Add files via upload

--- tp.c
+++ tp.c
@@ -10,19 +10,36 @@
 #define BOMBA "./Bomba"
 #define LECTURA 0
 #define ESCRITURA 1
-const int ERROR_FORK = -1;
-const int ERROR_EXECL = 1;
-
+const int ERROR = -1;
+#define FORMATO_PATH "/proc/%d/maps"
+#define FORMATO_ESCRITURA "r"
 #define TAMANIO_PIPE 2
 #define TAMANIO_BUF 64
+#define TAMANIO_PATH 64
+#define CARACTERES_CLAVE_STRING 16
+
+const int DIRECCION_CLAVE_1 = 0x1738;
+const int DIRECCION_CLAVE_2 = 0x1c;
+const int DIRECCION_CLAVE_3 = 0x17f7;
+const int DIRECCION_CLAVE_4 = 0x1877;
+const int DIRECCION_CLAVE_5 = 0x18d9;
+const int DIRECCION_CLAVE_6 = 0x3c;
 
  /*
-  * Precondiciones: -
-  * Postcondiciones: (Lanza la bomba como proceso hijo trazado por ptrace,
-  * redirigiendo su stdin a un pipe que controlamos.)
+  * Pre-condiciones:    pipe_fd no debe ser null.
+  * Post-condiciones:   Retorna el identificador de proceso para trazar con ptrace.
   */
-pid_t launch_bomb(int *pipe_fd) {
-    pipe(pipe_fd);
+pid_t lanzar_proceso_bomba(int *pipe_fd) {
+    if(!pipe_fd) {
+        printf("Error en la función lanzar proceso. pipe_fd es null.");
+        return ERROR;
+    }
+
+    if(pipe(pipe_fd) == ERROR) {
+        printf("Error en la función lanzar proceso. Falló la función pipe().");
+        return ERROR;
+    }
+
     pid_t pid = fork();
     if (pid == 0) {
         dup2(pipe_fd[LECTURA], STDIN_FILENO);
@@ -30,180 +47,212 @@ pid_t launch_bomb(int *pipe_fd) {
         close(pipe_fd[ESCRITURA]);
         ptrace(PTRACE_TRACEME, 0, NULL, NULL);
         execl(BOMBA, BOMBA, NULL);
-        perror("execl falló en la función launch_bomb.");
-        exit(ERROR_EXECL);// cambio por constante.
+        perror("execl falló en la función lanzar_proceso_bomba.");
+        exit(ERROR);
     }
     close(pipe_fd[LECTURA]);
     return pid;
 }
 
-/* Escribe una respuesta + salto de linea en el stdin de la bomba,
- * y la muestra en pantalla para que el output quede legible. */
-void inject_answer(int *pipe_fd, const char *answer) {
-    write(pipe_fd[ESCRITURA], answer, strlen(answer));
+ /*
+  * Pre-condiciones:    pipe_fd y respuesta no deben ser null.
+  * Post-condiciones:   Escribe la respuesta recibida stdin de la bomba e imprime por pantalla un 
+  *                     mensaje indicando que se inyectó la clave.
+  */
+void escribir_respuesta(int *pipe_fd, const char *respuesta) {
+    if(!pipe_fd || !respuesta) {
+        return;
+    }
+    write(pipe_fd[ESCRITURA], respuesta, strlen(respuesta));
     write(pipe_fd[ESCRITURA], "\n", 1);
+    printf("Clave inyectada.\n\n");
 }
 
-/* Lee la direccion base de carga del ejecutable (PIE) desde /proc/pid/maps */ 
-unsigned long get_base_address(pid_t pid) {
-    char path[64];
-    snprintf(path, sizeof(path), "/proc/%d/maps", pid);
-    FILE *f = fopen(path, "r");
+ /*
+  * Pre-condiciones:    -
+  * Post-condiciones:   Devuelve la dirección base de carga del ejecutable desde /proc/pid/maps.
+  */
+unsigned long obtener_direccion_base(pid_t pid) {
+    char path[TAMANIO_PATH];
+    snprintf(path, sizeof(path), FORMATO_PATH, pid);
+    FILE *archivo = fopen(path, FORMATO_ESCRITURA);
+    if(!archivo) {
+        printf("Error al abrir el archivo.");
+        return 0;
+    }
     unsigned long base = 0;
-    fscanf(f, "%lx-", &base);
-    fclose(f);
+    fscanf(archivo, "%lx-", &base);
+    fclose(archivo);
     return base;
 }
 
-/* Inserta un breakpoint (0xCC / int3) en addr, devuelve el byte original */
-long set_breakpoint(pid_t pid, unsigned long addr) {
-    long orig = ptrace(PTRACE_PEEKDATA, pid, (void*)addr, NULL);
-    long trap = (orig & ~0xFF) | 0xCC;
-    ptrace(PTRACE_POKEDATA, pid, (void*)addr, (void*)trap);
-    return orig;
+ /*
+  * Pre-condiciones:    -
+  * Post-condiciones: Frena la ejecución en la direccion dada
+  */
+long set_breakpoint(pid_t pid, unsigned long direccion) {
+    long original = ptrace(PTRACE_PEEKDATA, pid, (void*)direccion, NULL);
+    long trap = (original & ~0xFF) | 0xCC;
+    ptrace(PTRACE_POKEDATA, pid, (void*)direccion, (void*)trap);
+    return original;
 }
 
-/* Restaura el byte original, eliminando el breakpoint */
-void remove_breakpoint(pid_t pid, unsigned long addr, long orig) {
-    ptrace(PTRACE_POKEDATA, pid, (void*)addr, (void*)orig);
+ /*
+  * Pre-condiciones:    -
+  * Post-condiciones:   Elimina el breakpoint
+  */
+void remove_breakpoint(pid_t pid, unsigned long direccion, long original) {
+    ptrace(PTRACE_POKEDATA, pid, (void*)direccion, (void*)original);
 }
 
-/* Continua la ejecucion hasta golpear el breakpoint en bp_addr.
+ /*
+  * Pre-condiciones:    regs no debe ser null.
+  * Post-condiciones: (Continua la ejecución hasta llegar al breakpoint en bp_addr.
  * Al volver: deja los registros en regs, restaura el byte original
- * y corrige RIP para que apunte de nuevo a bp_addr (sin el 0xCC). */
-void run_to_breakpoint(pid_t pid, unsigned long bp_addr, long orig,
-                       struct user_regs_struct *regs) {
+ * y corrige RIP para que apunte de nuevo a bp_addr (sin el 0xCC).)
+  */
+void run_to_breakpoint(pid_t pid, unsigned long bp_addr, long original, struct user_regs_struct *regs) {
+    if(!regs) {
+        printf("Se recibe un puntero null en run_to_breakpoint.");
+        return;
+    }
     int status;
     ptrace(PTRACE_CONT, pid, NULL, NULL);
     waitpid(pid, &status, 0);
     ptrace(PTRACE_GETREGS, pid, NULL, regs);
-    remove_breakpoint(pid, bp_addr, orig);
+    remove_breakpoint(pid, bp_addr, original);
     regs->rip = bp_addr;
     ptrace(PTRACE_SETREGS, pid, NULL, regs);
 }
 
+ /*
+  * Pre-condiciones:    -
+  * Post-condiciones:   Imprime por pantalla el valor de la clave con su número correspondiente.
+  */
+void mostrar_clave(int numero_clave, char *valor) {
+    printf("Clave %i: %s\n", numero_clave, valor);
+}
+
+ /*
+  * Pre-condiciones:    regs no debe ser null.
+  *                     direccion es el valor que se debe sumar a la base para obtener el bp.
+  * Post-condiciones:   Devuelve el entero almacenado en el registro r15d, al momento en que se llega al bp.
+  */
+int obtener_clave_entero_1(pid_t pid, unsigned long base, struct user_regs_struct *regs, int direccion) {
+    if(!regs) {
+        return ERROR;
+    }
+    unsigned long bp = base + direccion;
+    long original = set_breakpoint(pid, bp);
+    run_to_breakpoint(pid, bp, original, regs);
+
+    return (int)regs->r15;
+}
+
+ /*
+  * Pre-condiciones:    regs no debe ser null.
+  *                     direccion es el valor que se debe sumar a la base para obtener el bp.
+  * Post-condiciones:   Devuelve el entero almacenado en el registro rbx, al momento en que se llega al bp.
+  */
+int obtener_clave_entero_3_5(pid_t pid, unsigned long base, struct user_regs_struct *regs, int direccion) {
+    if(!regs) {
+        return ERROR;
+    }
+    unsigned long bp = base + direccion; 
+    long original = set_breakpoint(pid, bp);
+    run_to_breakpoint(pid, bp, original, regs);
+    return (int)regs->rbx;
+}
+
+ /*
+  * Pre-condiciones:    regs no debe ser null.
+  *                     direccion es el valor que se debe sumar al rsp para obtener el bp.
+  * Post-condiciones:   Devuelve el entero almacenado en el registro rbx, al momento en que se llega al bp.
+  */
+float obtener_clave_float(pid_t pid, struct user_regs_struct *regs, int direccion) {
+    if(!regs) {
+        return ERROR;
+    }
+    unsigned long addr = regs->rsp + direccion;
+    long raw = ptrace(PTRACE_PEEKDATA, pid, (void*)addr, NULL);
+    float clave;
+    memcpy(&clave, &raw, sizeof(float));
+    return clave;
+}
+
+ /*
+  * Pre-condiciones:    clave y regs no deben ser null.
+  *                     direccion es el valor que se debe sumar a la base para obtener el bp.
+  * Post-condiciones:   Copia la clave encontrada en el parámetro clave recibido.
+  */
+void obtener_clave_string(pid_t pid, unsigned long base, struct user_regs_struct *regs, char *clave, int direccion) {
+    if(!regs || !clave) {
+        return;
+    }
+     unsigned long bp = base + direccion;
+    long original = set_breakpoint(pid, bp);
+    run_to_breakpoint(pid, bp, original, regs);
+
+    for (int i = 0; i < CARACTERES_CLAVE_STRING; i++) {
+        unsigned long addr = regs->rsp + 0x50 + i;
+        long letra = ptrace(PTRACE_PEEKDATA, pid, (void*)addr, NULL);
+        memcpy(clave + i, &letra, 1);
+    }
+}
+
 //----------------------------------------------------------------------------------------
+
 int main() {
     int pipe_fd[TAMANIO_PIPE];
 
-    pid_t pid = launch_bomb(pipe_fd);
-    if(pid==ERROR_FORK) {
-        printf("Error al obtener el proccess ID.");
-        return pid;
+    pid_t pid = lanzar_proceso_bomba(pipe_fd);
+    if(pid==ERROR) {
+        printf("Error al obtener el identificador de proceso.");
+        return ERROR;
     }
 
     int status;
-    waitpid(pid, &status, 0); // SIGTRAP inicial post-execve // por qué 0?
+    waitpid(pid, &status, 0);
 
-    unsigned long base = get_base_address(pid);
+    unsigned long base = obtener_direccion_base(pid);
 
     struct user_regs_struct regs;
     char buf[TAMANIO_BUF];
-    long orig;
-    unsigned long bp;
-    unsigned long addr;
-    long raw;
 
-    /* ============================================================
-     *  FASE 1 (entero)
-     *  Breakpoint en 0x1735: justo despues de "xor %r14d,%r15d". //por qué 0x1735??
-     *  La clave queda en r15 = r14 ^ r15 (valor previo).
-     * ============================================================ */
-    bp = base + 0x1738;// se pone el breakpoint en 1738 porque es donde r15 ya está modificado y contiene la clave
-    orig = set_breakpoint(pid, bp);
-    run_to_breakpoint(pid, bp, orig, &regs);
+    int clave_1 = obtener_clave_entero_1(pid, base, &regs, DIRECCION_CLAVE_1);
+    snprintf(buf, sizeof(buf), "%d", clave_1);
+    mostrar_clave(1, buf);
+    escribir_respuesta(pipe_fd, buf);
 
-    int clave1 = (int)regs.r15;
-    printf("\nClave fase 1: %d\n", clave1);
+    float clave_2 = obtener_clave_float(pid, &regs, DIRECCION_CLAVE_2);
+    snprintf(buf, sizeof(buf), "%f", clave_2);
+    mostrar_clave(2, buf);
+    escribir_respuesta(pipe_fd, buf);
 
-    snprintf(buf, sizeof(buf), "%d", clave1);
-    inject_answer(pipe_fd, buf);
+    int clave_3 = obtener_clave_entero_3_5(pid, base, &regs, DIRECCION_CLAVE_3);
+    snprintf(buf, sizeof(buf), "%d", clave_3);
+    mostrar_clave(3, buf);
+    escribir_respuesta(pipe_fd, buf);
 
-    /* ============================================================
-     *  FASE 2 (float)
-     *  El valor esperado ya esta calculado desde la inicializacion
-     *  y guardado en 0x1c(%rsp). Como rsp es estable, lo leemos
-     *  usando el mismo rsp del breakpoint de fase 1 (sin nuevo bp).
-     * ============================================================ */
-    addr = regs.rsp + 0x1c;
-    raw = ptrace(PTRACE_PEEKDATA, pid, (void*)addr, NULL); // en raw quedan un valor de tipo long en hexa y necesitamos float.
-    float clave2;
-    memcpy(&clave2, &raw, sizeof(float));
-    printf("\nClave fase 2: %f\n", clave2);
-    snprintf(buf, sizeof(buf), "%f", clave2);
-    inject_answer(pipe_fd, buf);
+    char clave_4[CARACTERES_CLAVE_STRING];
+    obtener_clave_string(pid, base, &regs, clave_4, DIRECCION_CLAVE_4);
+    mostrar_clave(4, clave_4);
+    escribir_respuesta(pipe_fd, clave_4);
 
-    /* ============================================================
-     *  FASE 3 (entero)
-     *  Breakpoint en 0x17f7: justo despues de calcular ebx
-     *  (clave fase 3), antes del fgets de fase 3.
-     *  Lo ponemos ANTES de soltar el proceso para evitar
-     *  condiciones de carrera con PTRACE_POKEDATA.
-     * ============================================================ */
-    bp = base + 0x17f7; //por qué 0x17f7??
-    orig = set_breakpoint(pid, bp);
-    run_to_breakpoint(pid, bp, orig, &regs);
+    int clave_5 = obtener_clave_entero_3_5(pid, base, &regs, DIRECCION_CLAVE_5);
+    snprintf(buf, sizeof(buf), "%d", clave_5);
+    mostrar_clave(5, buf);
+    escribir_respuesta(pipe_fd, buf);
 
-    int clave3 = (int)regs.rbx;
-    printf("\nClave fase 3: %d\n", clave3);
+    float clave_6 = obtener_clave_float(pid, &regs, DIRECCION_CLAVE_6);
+    snprintf(buf, sizeof(buf), "%f", clave_6);
+    mostrar_clave(6, buf);
+    escribir_respuesta(pipe_fd, buf);
 
-    snprintf(buf, sizeof(buf), "%d", clave3);
-    inject_answer(pipe_fd, buf);
-
-    /* ============================================================
-     *  FASE 4 (texto)
-     *  Breakpoint en 0x1877: justo despues de construir el string
-     *  esperado en 0x50(rsp)..0x5f(rsp) (16 bytes, null-terminated).
-     * ============================================================ */
-    bp = base + 0x1877;
-    orig = set_breakpoint(pid, bp);
-    run_to_breakpoint(pid, bp, orig, &regs);
-
-    char clave4[16] = {0}; //còmo sabemos que el string es de tamaño 16 (15 caracteres + /0)?
-    for (int i = 0; i < 16; i += 8) { // 2 veces
-        unsigned long addr4 = regs.rsp + 0x50 + i;
-        long word = ptrace(PTRACE_PEEKDATA, pid, (void*)addr4, NULL);
-        memcpy(clave4 + i, &word, 8);
-    }
-    printf("\nClave fase 4: %s\n", clave4);
-
-    inject_answer(pipe_fd, clave4);
-
-    /* ============================================================
-     *  FASE 5 (entero)
-     *  Breakpoint en 0x18d9: justo despues de calcular ebx
-     *  (clave5 = 0x2c(rsp) XOR 0x10(rsp) + r12d XOR r13d),
-     *  antes del fgets de fase 5.
-     * ============================================================ */
-    bp = base + 0x18d9;
-    orig = set_breakpoint(pid, bp);
-    run_to_breakpoint(pid, bp, orig, &regs);
-
-    int clave5 = (int)regs.rbx;
-    printf("\nClave fase 5: %d\n", clave5);
-
-    snprintf(buf, sizeof(buf), "%d", clave5);
-    inject_answer(pipe_fd, buf);
-
-    /* ============================================================
-     *  FASE 6 (float)
-     *  Igual que fase 2: el valor esperado en 0x3c(rsp) ya esta
-     *  calculado desde la inicializacion, rsp es estable.
-     * ============================================================ */
-    addr = regs.rsp + 0x3c;
-    raw = ptrace(PTRACE_PEEKDATA, pid, (void*)addr, NULL);
-    float clave6;
-    memcpy(&clave6, &raw, sizeof(float));
-    printf("\nClave fase 6: %f\n", clave6); 
-    snprintf(buf, sizeof(buf), "%f", clave6);
-    inject_answer(pipe_fd, buf);
-
-    printf("\n====================\n\n");
     fflush(stdout);
-
     ptrace(PTRACE_CONT, pid, NULL, NULL);
 
-    int w;// qué representa w?
+    int w;
     do {
         waitpid(pid, &w, 0);
     } while (!WIFEXITED(w) && !WIFSIGNALED(w));
